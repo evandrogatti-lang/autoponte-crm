fa66df5831efece8403447c474a7668579761290
evandrogatti-lang <evandro.gatti@gmail.com>
2026-08-20 17:47:10 +0200

    feat: mount theme comparison control

--- app/home-vision/page.tsx
+++ app/home-vision/page.tsx
@@ -1,7 +1,13 @@
 import { listPublicMarketplaceVehicles } from "../../lib/marketplace/public-vehicles";
 import AutoPonteExperience from "./experience";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 export default async function HomeVisionPreview() {
   const vehicles = await listPublicMarketplaceVehicles(20);
-  return <AutoPonteExperience initialVehicles={vehicles} />;
+  return (
+    <>
+      <AutoPonteExperience initialVehicles={vehicles} />
+      <ThemeSwitcher />
+    </>
+  );
 }
